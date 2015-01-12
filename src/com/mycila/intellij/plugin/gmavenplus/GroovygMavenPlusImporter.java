f27eae069a3d085b7f1ec8a3efd1e7836ce43b80
mathieucarbou <mathieu.carbou@gmail.com>
2015-01-12 18:34:31 -0500

    updated for intellij 14.x

--- src/com/mycila/intellij/plugin/gmavenplus/GroovygMavenPlusImporter.java
+++ src/com/mycila/intellij/plugin/gmavenplus/GroovygMavenPlusImporter.java
@@ -6,8 +6,8 @@ import org.jetbrains.idea.maven.importing.GroovyImporter;
  * @author Mathieu Carbou (mathieu.carbou@gmail.com)
  * @date 2014-04-13
  */
-public class GMavenPlusgroovyImporter extends GroovyImporter {
-    public GMavenPlusgroovyImporter() {
+public class GroovygMavenPlusImporter extends GroovyImporter {
+    public GroovygMavenPlusImporter() {
         super("org.codehaus.gmavenplus", "gmavenplus-plugin");
     }
 }
